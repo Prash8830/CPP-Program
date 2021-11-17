747d164db4854039b86f99433b63578e45d8795c
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-11-17 19:07:37 +0530

    done

--- onlinejudge/10038 - Jolly Jumpers.cpp
+++ onlinejudge/10038 - Jolly Jumpers.cpp
@@ -1,3 +1,6 @@
+//verdict: accepted ✔️
+// link : https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=979
+
 #include <bits/stdc++.h>
 using namespace std;
 
--- onlinejudge/10050 Hartals.cpp
+++ onlinejudge/10050 Hartals.cpp
@@ -1,3 +1,7 @@
+//verdict: accepted ✔️
+// link : https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=991
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
