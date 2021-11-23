1459d0a1dad067d3c4a40c415135de9144134a27
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-11-24 00:32:57 +0530

    WERTYU accepted

---
+++ onlinejudge/10082 _WERTYU.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    string s1="`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+    
+    string s2;
+    int n;
+    
+    while(getline(cin, s2))
+    {
+        string ans="";
+        for(int i=0;i<s2.size();i++)
+        {        
+            if(s2[i]!='Q' && s2[i]!='A' && s2[i]!='Z')
+            {
+                n=s1.find(s2[i]);
+                if(n!=-1)
+                {
+                    ans.append(s1,n-1,1); //append 1 character
+                }else ans.append(s2,i,1);//append space 
+            }else ans.append(s2,i,1);//append space
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
