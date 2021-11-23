ca35061bbed87790a7f2c0fc2ca72885675567b5
Prashant Rohidas Patil <80741792+Prash8830@users.noreply.github.com>
2021-11-23 14:38:09 +0530

    Create 10258_Contest_Scoreboard.cpp

---
+++ onlinejudge/10258_Contest_Scoreboard.cpp
@@ -0,0 +1,26 @@
+//10258 Contest Scoreboard
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int t; //testcases
+	cin>>t;
+	while(t--)
+	{
+	    int c,q,p; // c: CONTESTANT NO , q: QUESTION NO , p: penalty time
+	    string s; //Status of online judge on question
+	    vector<>contestant();
+	    
+	    while(cin>>c>>q>>p>>s)
+	    {
+	        
+	    }
+	}
+	
+	return 0;
+}
+
+//Output:
+//1 2 66
+//3 1 11
