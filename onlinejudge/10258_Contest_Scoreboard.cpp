58486897d5dea2109f5a4dce07faa28a42dab5a2
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-11-23 20:36:40 +0530

    phase1

--- onlinejudge/10258_Contest_Scoreboard.cpp
+++ onlinejudge/10258_Contest_Scoreboard.cpp
@@ -10,11 +10,21 @@ int main() {
 	{
 	    int c,q,p; // c: CONTESTANT NO , q: QUESTION NO , p: penalty time
 	    string s; //Status of online judge on question
-	    vector<>contestant();
-	    
+	    unordered_map<int,pair<int,int>>contestant;
+	    vector<vector<int>>status(101,vector<int>(10));
+		pair<int,int>a(0,0);
 	    while(cin>>c>>q>>p>>s)
 	    {
-	        
+	        if(contestant.find(c)==contestant.end()) contestant[c]=a;  //if contestant is new
+			if(s=="I")
+			{
+				status[c][q]+=20; //penalty of 20min for wrong solution
+			}
+			else if(s=="C")
+			{
+				contestant[c].first+=1;
+				contestant[c].second+=p+status[c][q];
+			}
 	    }
 	}
 	
