19f02ee10c99e898f7dfce01e7b839da6cfc8fff
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-11-24 00:00:40 +0530

    succesful submission

--- onlinejudge/10258_Contest_Scoreboard.cpp
+++ onlinejudge/10258_Contest_Scoreboard.cpp
@@ -2,6 +2,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<string> divide(string s)
+{
+    vector<string> a;
+    string k;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (!isspace(s[i]))
+            k = k + s[i];
+        else
+        {
+            a.push_back(k);
+            k = "";
+        }
+    }
+    if (k.size() != 0)
+        a.push_back(k);
+    return a;
+}
+
 bool compare(pair<int,pair<int,int>>a,pair<int,pair<int,int>>b)
 {
 	if(a.second.first>b.second.first) return true;
@@ -20,6 +39,7 @@ int main() {
 	
 	int t; //testcases
 	cin>>t;
+	bool f=0;
 	while(t--)
 	{
 	    int c,q,p; // c: CONTESTANT NO , q: QUESTION NO , p: penalty time
@@ -27,8 +47,24 @@ int main() {
 	    unordered_map<int,pair<int,int>>contestant;
 	    vector<vector<int>>status(101,vector<int>(10));
 		pair<int,int>a(0,0);
-	    while(cin>>c>>q>>p>>s)
+		string command; // to understand end of testcases
+		if(f==0) //to avoid line after no indicating testcase and line after that 
+		{
+		    getline(cin, command);
+		    getline(cin, command);
+		    f=1;
+		}
+	    while(getline(cin, command))
 	    {
+			// getline(cin, command);
+			vector<string> ans;
+        	ans = divide(command);
+			if(ans.size()==0)break;			
+			c=stoi(ans[0]);
+			q=stoi(ans[1]);
+			p=stoi(ans[2]);
+			s=ans[3];
+
 	        if(s=="C" || s=="I" || s=="R" || s=="U" || s=="E") if(contestant.find(c)==contestant.end()) contestant[c]=a;  //if contestant is new
 			if(s=="I")
 			{
@@ -61,7 +97,7 @@ int main() {
 			cout<<answer[i].first<<" "<<answer[i].second.first<<" "<<answer[i].second.second<<"\n";
 			//  contestant no            no of problem solved         penalty time
 		}
-		cout << "\n";
+		if(t) cout << "\n";
 	}
 	
 	return 0;
